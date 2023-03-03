63d719b1156a9c9560c332db48f204605a536539
SebastianSlezak <szalassje@gmail.com>
2023-03-03 22:11:32 +0100

    Draft aside

---
+++ frontend/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <aside>
+    <div class="logo">
+
+    </div>
+
+    <div class="homepage">
+
+    </div>
+
+    <div class="more">
+
+    </div>
+  </aside>
+</template>
+
+<style>
+aside {
+  width: 244px;
+  height: 100vh;
+  padding: 8px 12px 20px;
+  background-color: rgb(0, 0, 0);
+}
+
+.logo {
+  width: 220px;
+  height: 92px;
+  background-color: aquamarine;
+}
+
+.homepage {
+  width: 220px;
+  height: 537.600px;
+  background-color: bisque;
+}
+
+.more {
+  width: 220px;
+  height: 56px;
+  background-color: blue;
+}
+
+body {
+  margin: 0;
+}
+
+</style>
