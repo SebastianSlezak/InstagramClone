a05d9d254c32cf4cc6fb27a99af2c96cc2cb7509
SebastianSlezak <szalassje@gmail.com>
2023-03-04 23:28:31 +0100

    Added border in the aside and positioning of divs

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -15,11 +15,20 @@
 </template>
 
 <style>
+body {
+  background-color: black;
+}
+
 aside {
+  display: flex;
+  flex-direction: column;
   width: 244px;
   height: 100vh;
   padding: 8px 12px 20px;
   background-color: rgb(0, 0, 0);
+  border-right-color: rgb(38, 38, 38);
+  border-right-style: solid;
+  border-right-width: 0.8px;
 }
 
 .logo {
